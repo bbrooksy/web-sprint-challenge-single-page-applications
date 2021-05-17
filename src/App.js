93fedf3257a133e50080f0c4c91d03004f7b5e01
bbrooksy <72233725+bbrooksy@users.noreply.github.com>
2021-05-17 01:12:37 -0400

    Finished as much as possible

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
+import FormInput from "./Form";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+
+
+function Home() {
+  return (
+  <div>
+  <h1>Lambda Eats</h1>
+  <img src='https://unsplash.com/photos/MqT0asuoIcU?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink'/>
+  </div>
+  )
+}
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='App'>
+    <BrowserRouter>
+  <div className='links'>
+    <Link to='/'>Home</Link>
+    <Link to='/Form'>Order Now</Link>
+</div>
+  <div className='paths'>
+    
+    <Route exact path='/' component={Home} />
+    <Route path='/Form' component={FormInput}/>
+</div>
+    </BrowserRouter>
+</div>
+    
+      
+    
   );
 };
 export default App;
